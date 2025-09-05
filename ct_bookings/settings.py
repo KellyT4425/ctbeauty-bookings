4c12df62f5be49ca38850e56eed3a0037fbce0b2
Kelly Thompson <7luwcuip@students.codeinstitute.net>
2025-09-05 11:51:03 +0100

    fix(static): ensure CSS changes reflect in dev by disabling collectstatic usage

--- ct_bookings/settings.py
+++ ct_bookings/settings.py
@@ -201,9 +201,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 
 AUTHENTICATION_BACKENDS = [
